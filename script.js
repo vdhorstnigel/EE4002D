9d093f3ac8111ec680168f809a26b996f23f7a5c
Nigel <aquanat12@gmail.com>
2025-04-08 22:50:38 +0800

    edit

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ client.on('message', (topic, message) => {
     console.log('Received message on topic:', topic, 'with data:', data); // Log the received message
 
     if (topic === 'esp32/values') {
-      console.log('Updating sensor values, ${data.Ethanol}');
       document.querySelector('#ethanol .value').textContent = `${data.Ethanol} ppm`;
       document.querySelector('#ammonia .value').textContent = `${data.Ammonia} ppm`;
       document.querySelector('#hydrogen-sulfide .value').textContent = `${data["Hydrogen Sulfide"]} ppm`;
