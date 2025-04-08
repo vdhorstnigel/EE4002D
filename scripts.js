b6de6aff9ae6cc079f0cc4030adc4ecd8f324367
Nigel <aquanat12@gmail.com>
2025-04-08 21:40:13 +0800

    Initial Draft

---
+++ scripts.js
@@ -0,0 +1,22 @@
+// MQTT Setup (using WebSocket)
+const client = mqtt.connect('wss://x2124b00.ala.asia-southeast1.emqxsl.com:8883/esp32', {
+    username: 'ESP32',
+    password: 'Password1234!'
+  });
+  
+// Update UI when MQTT message arrives
+client.on('message', (topic, message) => {
+    const data = JSON.parse(message.toString());
+    
+    // Update each sensor box
+    document.querySelector('#ethanol .value').textContent = `${data.Ethanol} ppm`;
+    document.querySelector('#ammonia .value').textContent = `${data.Ammonia} ppm`;
+    document.querySelector('#hydrogen-sulfide .value').textContent = `${data["Hydrogen Sulfide"]} ppm`;
+    document.querySelector('#ethylene .value').textContent = `${data.Ethylene} ppm`;
+  });
+  
+  // Subscribe to topics
+  client.on('connect', () => {
+    client.subscribe('values');
+    console.log('Connected to MQTT broker!');
+  });
